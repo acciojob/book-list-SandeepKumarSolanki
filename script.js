0ef1a963a1a298812217b6e877aa5dbfe4774330
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-03-08 12:59:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+
+let form = document.querySelector('form');
+let title = document.getElementById('title')
+let author = document.getElementById('author')
+let isbn = document.getElementById('isbn');
+let table = document.getElementsByClassName('table')
+let deleteBtn = document.getElementById('deleteRow');
+form.addEventListener("submit" , ()=>{
+	
+	let newRow = table.insertRow();
+	let cell1 = newRow.insertCell();
+	cell1.innerText = `${title.value}`
+	let cell2 = newRow.insertCell();
+	cell2.innerText = `${author.value}`
+	let cell3 = newRow.insertCell();
+	cell1.innerText = `${isbn.value}`
+
+	deleteBtn.addEventListener("click" , ()=>{
+		let row = document.
+		row.removeItem();
+	})
+	
+})
